224722637a4b3bc3968486e073c76665664281fc
Rongmario <ethanmahmood@gmail.com>
2021-05-12 17:04:26 +0100

    Fixes #2 (no version bump)

--- src/main/java/zone/rong/mixinbooter/loader/mixin/LoadControllerMixin.java
+++ src/main/java/zone/rong/mixinbooter/loader/mixin/LoadControllerMixin.java
@@ -1,11 +1,11 @@
 package zone.rong.mixinbooter.loader.mixin;
 
 import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.fml.common.LoadController;
-import net.minecraftforge.fml.common.LoaderState;
+import net.minecraftforge.fml.common.*;
 import net.minecraftforge.fml.common.discovery.ASMDataTable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,24 +20,27 @@ import java.lang.reflect.Method;
 @Mixin(value = LoadController.class, remap = false)
 public class LoadControllerMixin {
 
+    @Shadow private Loader loader;
+
     @Inject(method = "distributeStateMessage(Lnet/minecraftforge/fml/common/LoaderState;[Ljava/lang/Object;)V", at = @At("HEAD"))
     private void beforeConstructing(LoaderState state, Object[] eventData, CallbackInfo ci) throws Throwable {
         if (state == LoaderState.CONSTRUCTING) { // This state is where Forge adds mod files to ModClassLoader
 
-            // Field allConfigs = Config.class.getDeclaredField("allConfigs"); // Mixins::getConfigs is broken
-            // allConfigs.setAccessible(true);
-            // Set<Config> vanillaConfigs = new ObjectOpenHashSet<>((Collection<Config>) ((Map) allConfigs.get(null)).values());
-
+            ModClassLoader modClassLoader = (ModClassLoader) eventData[0];
             ASMDataTable asmDataTable = (ASMDataTable) eventData[1];
+
             MixinBooterPlugin.LOGGER.info("Instantiating all MixinLoader annotated classes...");
 
             for (ASMDataTable.ASMData asmData : asmDataTable.getAll(MixinLoader.class.getName())) {
+                modClassLoader.addFile(asmData.getCandidate().getModContainer()); // Add to path before `newInstance`
                 Class<?> clazz = Class.forName(asmData.getClassName());
                 MixinBooterPlugin.LOGGER.info("Instantiating {} for its mixins.", clazz);
                 clazz.newInstance();
             }
 
-            // Set<Config> moddedConfigs = Sets.difference(new ObjectOpenHashSet<>((Collection<Config>) ((Map) allConfigs.get(null)).values()), vanillaConfigs);
+            for (ModContainer container : this.loader.getActiveModList()) {
+                modClassLoader.addFile(container.getSource());
+            }
 
             Field transformerField = Proxy.class.getDeclaredField("transformer");
             transformerField.setAccessible(true);
@@ -56,9 +59,6 @@ public class LoadControllerMixin {
             Method prepareConfigsMethod = MixinProcessor.class.getDeclaredMethod("prepareConfigs", MixinEnvironment.class);
             prepareConfigsMethod.setAccessible(true);
             prepareConfigsMethod.invoke(processor, env);
-
-            // Set<String> moddedMixinTargets = moddedConfigs.stream().map(Config::getConfig).map(IMixinConfig::getTargets).flatMap(Collection::stream).collect(Collectors.toSet());
-
         }
     }
 
