cdaecf91ccbf3a3e7e03bbc88309924e7e0bc991
Korewa_Li <li.hvktqs@gmail.com>
2023-04-14 09:08:45 +0700

    Unsafe go away!

--- src/main/java/zone/rong/mixinextras/utils/ClassGenUtils.java
+++ src/main/java/zone/rong/mixinextras/utils/ClassGenUtils.java
@@ -2,11 +2,8 @@ package zone.rong.mixinextras.utils;
 
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
-import sun.misc.Unsafe;
 
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 
@@ -16,29 +13,19 @@ public class ClassGenUtils {
     static {
         Definer theDefiner;
         try {
-            Unsafe.class.getMethod("defineClass", String.class, byte[].class, int.class, int.class, ClassLoader.class, ProtectionDomain.class);
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            Unsafe unsafe = (Unsafe) theUnsafe.get(null);
-            theDefiner = (name, bytes, scope) -> unsafe.defineClass(name, bytes, 0, bytes.length, scope.lookupClass().getClassLoader(), scope.lookupClass().getProtectionDomain());
-        } catch (IllegalAccessException | NoSuchFieldException | NoSuchMethodException e1) {
-            try {
-                //noinspection JavaReflectionMemberAccess
-                Method defineClass = MethodHandles.Lookup.class.getMethod("defineClass", byte[].class);
-                theDefiner = (name, bytes, scope) -> {
-                    try {
-                        //noinspection PrimitiveArrayArgumentToVarargsMethod
-                        defineClass.invoke(scope, bytes);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                };
-            } catch (NoSuchMethodException e2) {
-                RuntimeException e = new RuntimeException("Could not resolve class definer! Please inform to Rongmario.");
-                e.addSuppressed(e1);
-                e.addSuppressed(e2);
-                throw e;
-            }
+            Method method = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class, ProtectionDomain.class);
+            method.setAccessible(true);
+            theDefiner = (name, bytes, scope) -> {
+                try {
+                    scope.unreflect(method).invokeExact(scope.lookupClass().getClassLoader(), name.replace('/', '.'), bytes, 0, bytes.length);
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
+            };
+        } catch (NoSuchMethodException nsme) {
+            RuntimeException re = new RuntimeException("Could not resolve class definer! Please inform to Rongmario.");
+            re.addSuppressed(nsme);
+            throw re;
         }
         DEFINER = theDefiner;
     }
