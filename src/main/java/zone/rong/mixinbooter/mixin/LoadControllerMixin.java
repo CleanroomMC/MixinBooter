895b547209e759856cecbef067c51480a7083458
Rongmario <rongmario@proton.me>
2026-04-21 17:30:56 +0100

    Use Proxy#refreshMixins

--- src/main/java/zone/rong/mixinbooter/mixin/LoadControllerMixin.java
+++ src/main/java/zone/rong/mixinbooter/mixin/LoadControllerMixin.java
@@ -3,22 +3,15 @@ package zone.rong.mixinbooter.mixin;
 import net.minecraftforge.fml.common.*;
 import net.minecraftforge.fml.common.discovery.ASMDataTable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.extensibility.IMixinProcessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.transformer.IMixinTransformer;
 import org.spongepowered.asm.mixin.transformer.Proxy;
-import org.spongepowered.asm.service.MixinService;
-import org.spongepowered.asm.service.mojang.MixinServiceLaunchWrapper;
 import zone.rong.mixinbooter.*;
 import zone.rong.mixinbooter.fix.MixinFixer;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -107,15 +100,7 @@ public class LoadControllerMixin {
                 }
             }
 
-            // Rebuild delegated transformers
-            Field delegatedTransformersField = MixinServiceLaunchWrapper.class.getDeclaredField("delegatedTransformers");
-            delegatedTransformersField.setAccessible(true);
-            delegatedTransformersField.set(MixinService.getService(), null);
-
-            IMixinProcessor processor = ((IMixinTransformer) Proxy.transformer).getProcessor();
-            Method selectMethod = processor.getClass().getDeclaredMethod("select", MixinEnvironment.class);
-            selectMethod.setAccessible(true);
-            selectMethod.invoke(processor, MixinEnvironment.getCurrentEnvironment());
+            Proxy.refreshMixins();
         }
     }
 
