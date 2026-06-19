e27595c9816ff93077ce9a607bd7c07df03c1a39
Rongmario <rongmario@proton.me>
2026-06-19 22:46:41 +0100

    Removed try-catch when trying to find mixin metadata

--- src/main/java/zone/rong/mixinbooter/mixin/CrashReportMixin.java
+++ src/main/java/zone/rong/mixinbooter/mixin/CrashReportMixin.java
@@ -23,49 +23,44 @@ public class CrashReportMixin {
 
     @Inject(method = "getCauseStackTraceOrString", at = @At("RETURN"), cancellable = true)
     private void afterStackTracePopulation(CallbackInfoReturnable<String> cir, @Local(ordinal = 0) Throwable throwable) {
-        try {
-            Map<String, ClassInfo> classes = new LinkedHashMap<>();
-            while (throwable != null) {
-                if (throwable instanceof NoClassDefFoundError) {
-                    ClassInfo classInfo = ClassInfo.fromCache(throwable.getMessage());
-                    if (classInfo != null) {
-                        classes.put(throwable.getMessage(), classInfo);
-                    }
+        Map<String, ClassInfo> classes = new LinkedHashMap<>();
+        while (throwable != null) {
+            if (throwable instanceof NoClassDefFoundError) {
+                ClassInfo classInfo = ClassInfo.fromCache(throwable.getMessage());
+                if (classInfo != null) {
+                    classes.put(throwable.getMessage(), classInfo);
                 }
-                StackTraceElement[] stacktrace = throwable.getStackTrace();
-                for (StackTraceElement stackTraceElement : stacktrace) {
-                    String className = stackTraceElement.getClassName().replace('.', '/');
-                    if (classes.containsKey(className)) {
-                        ClassInfo classInfo = ClassInfo.fromCache(className);
-                        while (classInfo != null) {
-                            classes.put(className, classInfo);
-                            className = classInfo.getSuperName();
-                            if (className == null || className.isEmpty() || "java/lang/Object".equals(className)) {
-                                break;
-                            }
-                            classInfo = classInfo.getSuperClass();
+            }
+            StackTraceElement[] stacktrace = throwable.getStackTrace();
+            for (StackTraceElement stackTraceElement : stacktrace) {
+                String className = stackTraceElement.getClassName().replace('.', '/');
+                if (classes.containsKey(className)) {
+                    ClassInfo classInfo = ClassInfo.fromCache(className);
+                    while (classInfo != null) {
+                        classes.put(className, classInfo);
+                        className = classInfo.getSuperName();
+                        if (className == null || className.isEmpty() || "java/lang/Object".equals(className)) {
+                            break;
                         }
+                        classInfo = classInfo.getSuperClass();
                     }
                 }
-                throwable = throwable.getCause();
             }
-            if (classes.isEmpty()) {
-                cir.setReturnValue(cir.getReturnValue() + "\nNo Mixin Metadata is found in the Stacktrace.\n");
+            throwable = throwable.getCause();
+        }
+        if (classes.isEmpty()) {
+            cir.setReturnValue(cir.getReturnValue() + "\nNo Mixin Metadata is found in the Stacktrace.\n");
+        } else {
+            StringBuilder mixinMetadataBuilder = new StringBuilder("\nMixins in Stacktrace:");
+            boolean addedMetadata = false;
+            for (Map.Entry<String, ClassInfo> entry : classes.entrySet()) {
+                addedMetadata |= mixinbooter$findAndAddMixinMetadata(mixinMetadataBuilder, entry.getKey(), entry.getValue());
+            }
+            if (addedMetadata) {
+                cir.setReturnValue(cir.getReturnValue() + mixinMetadataBuilder);
             } else {
-                StringBuilder mixinMetadataBuilder = new StringBuilder("\nMixins in Stacktrace:");
-                boolean addedMetadata = false;
-                for (Map.Entry<String, ClassInfo> entry : classes.entrySet()) {
-                    addedMetadata |= mixinbooter$findAndAddMixinMetadata(mixinMetadataBuilder, entry.getKey(), entry.getValue());
-                }
-                if (addedMetadata) {
-                    cir.setReturnValue(cir.getReturnValue() + mixinMetadataBuilder);
-                } else {
-                    cir.setReturnValue(cir.getReturnValue() + "\nNo Mixin Metadata is found in the Stacktrace.\n");
-                }
+                cir.setReturnValue(cir.getReturnValue() + "\nNo Mixin Metadata is found in the Stacktrace.\n");
             }
-        } catch (Throwable t) {
-            MixinService.getService().getLogger(Tags.MOD_NAME).fatal("Unable to gather mixin metadata from the stacktrace", t);
-            cir.setReturnValue(cir.getReturnValue() + "\nFailed to find Mixin Metadata in Stacktrace due to exception: " + t);
         }
     }
 
