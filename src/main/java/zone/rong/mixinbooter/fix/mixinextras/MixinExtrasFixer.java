54af04a32594bc27370099521ac7ac2f54faf13e
Rongmario <rongmario@proton.me>
2026-07-05 11:08:36 +0100

    Fixes #124

--- src/main/java/zone/rong/mixinbooter/fix/mixinextras/MixinExtrasFixer.java
+++ src/main/java/zone/rong/mixinbooter/fix/mixinextras/MixinExtrasFixer.java
@@ -3,17 +3,12 @@ package zone.rong.mixinbooter.fix.mixinextras;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Handle;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
 import java.util.ListIterator;
 
 public class MixinExtrasFixer implements IClassTransformer, Opcodes {
-
-    public static Handle redirect(int tag, String owner, String name, String desc, boolean itf) {
-        return new Handle(tag, owner, name, desc);
-    }
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] classBytes) {
@@ -34,21 +29,22 @@ public class MixinExtrasFixer implements IClassTransformer, Opcodes {
 
         for (MethodNode method : node.methods) {
             ListIterator<AbstractInsnNode> iterator = method.instructions.iterator();
-            boolean foundNew = false;
-            boolean foundDup = false;
             while (iterator.hasNext()) {
                 AbstractInsnNode instruction = iterator.next();
-                if (!foundNew && !foundDup && instruction.getOpcode() == NEW &&
-                        "org/objectweb/asm/Handle".equals(((TypeInsnNode) instruction).desc)) {
-                    foundNew = true;
-                    iterator.remove();
-                } else if (foundNew && instruction.getOpcode() == DUP) {
-                    foundNew = false;
-                    foundDup = true;
-                    iterator.remove();
-                } else if (!foundNew && foundDup && instruction.getOpcode() == INVOKESPECIAL &&
-                        "org/objectweb/asm/Handle".equals(((MethodInsnNode) instruction).owner)) {
-                    iterator.set(new MethodInsnNode(INVOKESTATIC, "zone/rong/mixinbooter/fix/mixinextras/MixinExtrasFixer", "redirect", "(ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)Lorg/objectweb/asm/Handle;"));
+                if (instruction.getOpcode() == INVOKESPECIAL) {
+                    MethodInsnNode call = (MethodInsnNode) instruction;
+                    if ("org/objectweb/asm/Handle".equals(call.owner) &&
+                            "<init>".equals(call.name) &&
+                            "(ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Z)V".equals(call.desc)) {
+                        iterator.set(new InsnNode(POP));
+                        iterator.add(new MethodInsnNode(
+                                INVOKESPECIAL,
+                                "org/objectweb/asm/Handle",
+                                "<init>",
+                                "(ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
+                                false)
+                        );
+                    }
                 }
             }
         }
