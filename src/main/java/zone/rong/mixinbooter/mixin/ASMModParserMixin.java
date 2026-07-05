d84fab3a697765c8e49b482b89475bbd8f7f6ce9
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-07-05 14:53:54 +0800

    try using base Mixin instead of MixinExtra injector
    (cherry picked from commit 3ac91a44fea994a3952d7a2fa123e04bdd2d33bd)

--- src/main/java/zone/rong/mixinbooter/mixin/ASMModParserMixin.java
+++ src/main/java/zone/rong/mixinbooter/mixin/ASMModParserMixin.java
@@ -1,15 +1,15 @@
 package zone.rong.mixinbooter.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraftforge.fml.common.discovery.asm.ASMModParser;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Type;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import zone.rong.mixinbooter.fix.SuppressingClassReader;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @Mixin(value = ASMModParser.class, remap = false)
@@ -18,20 +18,17 @@ public class ASMModParserMixin {
     @Shadow
     private Type asmType;
 
-    @WrapOperation(method = "<init>", at = @At(value = "NEW", target = "org/objectweb/asm/ClassReader"))
-    private ClassReader suppressClassReaderWhenReadingNewClasses(InputStream is, Operation<ClassReader> original) {
-        ClassReader reader;
+    @Redirect(method = "<init>", at = @At(value = "NEW", target = "org/objectweb/asm/ClassReader"))
+    private ClassReader suppressClassReaderWhenReadingNewClasses(InputStream is) throws IOException {
         try {
-            reader = original.call(is);
+            return new ClassReader(is);
         } catch (IllegalArgumentException e) {
             if (e.getMessage() == null) {
                 this.asmType = Type.VOID_TYPE;
-                reader = new SuppressingClassReader();
-            } else {
-                throw e;
+                return new SuppressingClassReader();
             }
+            throw e;
         }
-        return reader;
     }
 
 }
